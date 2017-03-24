61ce17c9095dd8b3e617e6b01656cfbc10eaa9f2
Ronen Lapushner <ronenk17@gmail.com>
2017-03-25 00:42:25 +0300

    Changed amount of queries

--- devices/dht11.h
+++ devices/dht11.h
@@ -26,7 +26,7 @@
 #include "devicevals.h"
 #include "confighandler.h"
 
-#define DHT11_MAX_READS 				96
+#define DHT11_MAX_READS 				128
 #define DHT11_MAX_RESULT_STRING_LEN		8 /* xxx.xxx + '\0' ==> 8 characters */
 
 int dht11_query_callback(int data_pin, float *arr);
