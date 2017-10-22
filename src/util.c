120bc5789061577cf1580ddb4e49499f7e7702ac
Ronen Lapushner <ronenk17@gmail.com>
2017-10-22 10:28:40 +0300

    Fixed code formatting

--- src/util.c
+++ src/util.c
@@ -23,150 +23,150 @@ static const char *SUPPORTED_DATETIME_FORMATS[] = {
 };
 
 int write_pid_file(void) {
-	int fd = 0;
-	char temp_buffer[PID_NUMBER_BUFFER_LENGTH];
+    int fd = 0;
+    char temp_buffer[PID_NUMBER_BUFFER_LENGTH];
 
     if ((fd = open(PID_FILE, O_RDWR | O_CREAT | O_EXCL, 0444)) == -1)
-		return 0;
-	else {
-		/* Write PID */
-		sprintf(temp_buffer, "%ld", (long)getpid());
-		write(fd, temp_buffer, strlen(temp_buffer));
-	}
+        return 0;
+    else {
+        /* Write PID */
+        sprintf(temp_buffer, "%ld", (long)getpid());
+        write(fd, temp_buffer, strlen(temp_buffer));
+    }
 
-	return fd;
+    return fd;
 }
 
 int pid_file_exists(void) {
-	struct stat st;
-	int result = stat(PID_FILE, &st);
+    struct stat st;
+    int result = stat(PID_FILE, &st);
 
-	return result == 0;
+    return result == 0;
 }
 
 /* Conversion from rpiwd time units to seconds */
 unsigned int rpiwd_units_to_milliseconds(const char *unitstr) {
-	const char *ptr = unitstr;
-	char unit = '\0';
-	float count = -1, seconds = 1;
+    const char *ptr = unitstr;
+    char unit = '\0';
+    float count = -1, seconds = 1;
 
-	/* Iterate until the unit is found, or end of string. */
-	while (isdigit(*ptr) || *ptr == '.' && *ptr != '\0') { ptr++; }
-	if (*ptr)
-		unit = *ptr;
+    /* Iterate until the unit is found, or end of string. */
+    while (isdigit(*ptr) || *ptr == '.' && *ptr != '\0') { ptr++; }
+    if (*ptr)
+        unit = *ptr;
 
-	/* After the unit there should be no more tokens remaining.
-	 * If there are more characters than that, it is an error. */
-	if (*(ptr + 1) != '\0')
-		return 0;
+    /* After the unit there should be no more tokens remaining.
+     * If there are more characters than that, it is an error. */
+    if (*(ptr + 1) != '\0')
+        return 0;
 
-	/* Check unit */
-	switch (unit) {
-		case 's': /* Seconds */
-			break;
-		case 'm': /* Minutes */
-			seconds *= 60;
-			break;
-		case 'h': /* Hours */
-			seconds *= 60 * 60;
-			break;
-		case 'd': /* Days */
-			seconds *= 60 * 60 * 24;
-			break;
-		default: /* Unknown / missing */
-			return 0;
-	}
+    /* Check unit */
+    switch (unit) {
+        case 's': /* Seconds */
+            break;
+        case 'm': /* Minutes */
+            seconds *= 60;
+            break;
+        case 'h': /* Hours */
+            seconds *= 60 * 60;
+            break;
+        case 'd': /* Days */
+            seconds *= 60 * 60 * 24;
+            break;
+        default: /* Unknown / missing */
+            return 0;
+    }
 
-	/* Get count and check it. If it is correct, multiply it by value in seconds */
-	count = strtof(unitstr, NULL);
-	if (errno == ERANGE || count == 0)
-		return 0;
+    /* Get count and check it. If it is correct, multiply it by value in seconds */
+    count = strtof(unitstr, NULL);
+    if (errno == ERANGE || count == 0)
+        return 0;
 
-	/* Multiply */
-	seconds *= count * 1000;
+    /* Multiply */
+    seconds *= count * 1000;
 
-	return (unsigned int)seconds;
+    return (unsigned int)seconds;
 }
 
 char rpiwd_direction_to_char(const char *direction) {
-	if (strcmp(direction, DIRECTION_STRING_FROM) == 0)
-		return '>';
-	else if (strcmp(direction, DIRECTION_STRING_TO) == 0)
-		return '<';
-	else
-		return '?';
+    if (strcmp(direction, DIRECTION_STRING_FROM) == 0)
+        return '>';
+    else if (strcmp(direction, DIRECTION_STRING_TO) == 0)
+        return '<';
+    else
+        return '?';
 }
 
 int is_rpiwd_units(const char *str) {
-	float count = 0;
+    float count = 0;
 
-	/* Check for a number */
-	count = strtof(str, NULL);
-	if (count == 0 || errno == ERANGE)
-		return 0;
+    /* Check for a number */
+    count = strtof(str, NULL);
+    if (count == 0 || errno == ERANGE)
+        return 0;
 
-	return 1;
+    return 1;
 }
 
 void rpiwd_sleep(unsigned int milliseconds) {
-	struct timespec tms;
+    struct timespec tms;
 
-	/* Convert milliseconds back to seconds.
-	 * If anything remains, multiply and put in nanoseconds. */
-	tms.tv_sec = milliseconds / 1000;
-	tms.tv_nsec = milliseconds % 1000 * 1000000;
+    /* Convert milliseconds back to seconds.
+     * If anything remains, multiply and put in nanoseconds. */
+    tms.tv_sec = milliseconds / 1000;
+    tms.tv_nsec = milliseconds % 1000 * 1000000;
 
-	nanosleep(&tms, NULL);
+    nanosleep(&tms, NULL);
 }
 
 char *rpiwd_getline(const char *line, const char *newline) {
-	char *nline_pos = NULL, *buffer;
-	ptrdiff_t length;
+    char *nline_pos = NULL, *buffer;
+    ptrdiff_t length;
 
-	/* Find the newline */
-	nline_pos = strstr(line, newline);
-	if (!nline_pos)
-		return NULL; /* No newline */
+    /* Find the newline */
+    nline_pos = strstr(line, newline);
+    if (!nline_pos)
+        return NULL; /* No newline */
 
-	/* Get the difference. This is the length. */
-	length = nline_pos - line;
-	if (length <= 0)
-		return NULL; /* Empty string */
+    /* Get the difference. This is the length. */
+    length = nline_pos - line;
+    if (length <= 0)
+        return NULL; /* Empty string */
 
-	/* Allocate and copy memory */
-	buffer = malloc(sizeof(char) * length + 1);
-	if (!buffer)
-		return NULL;
+    /* Allocate and copy memory */
+    buffer = malloc(sizeof(char) * length + 1);
+    if (!buffer)
+        return NULL;
 
-	/* Copy and put newline */
-	strncpy(buffer, line, length);
-	buffer[length] = '\0';
+    /* Copy and put newline */
+    strncpy(buffer, line, length);
+    buffer[length] = '\0';
 
-	return buffer;
+    return buffer;
 }
 
 time_t rpiwd_units_to_time_t(const char *unitstr, char operation) {
-	time_t now = time(NULL);
+    time_t now = time(NULL);
 
-	/* Get milliseconds */
-	unsigned int millis = rpiwd_units_to_milliseconds(unitstr);
-	if (millis == 0)
-		return 0;
+    /* Get milliseconds */
+    unsigned int millis = rpiwd_units_to_milliseconds(unitstr);
+    if (millis == 0)
+        return 0;
 
-	/* Add/subtract */
-	switch (operation) {
-		case '+':
-			now += (time_t) millis / 1000;
-			break;
-		case '-':
-			now -= (time_t) millis / 1000;
-			break;
-		default:
-			now = 0;
-			break;
-	}
+    /* Add/subtract */
+    switch (operation) {
+        case '+':
+            now += (time_t) millis / 1000;
+            break;
+        case '-':
+            now -= (time_t) millis / 1000;
+            break;
+        default:
+            now = 0;
+            break;
+    }
 
-	return now;
+    return now;
 }
 
 
@@ -253,7 +253,7 @@ int rpiwd_is_number(const char *str) {
 /* NOTE: errno is never negative in getpwnam()'s case,
  * so no problem returning it instead of an actual uid_t! */
 int username_to_uid(const char *username, uid_t *resPtr) {
-	const struct passwd *rpiwdUser = getpwnam(username);
+    const struct passwd *rpiwdUser = getpwnam(username);
 
     /* Check if we have found the user */
     if (rpiwdUser) {
