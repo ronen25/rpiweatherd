8547e0be183c309a0218e76a5702153d69864f6d
Ronen Lapushner <ronenk17@gmail.com>
2017-10-22 10:36:24 +0300

    Fixed code formatting

--- include/http.h
+++ include/http.h
@@ -33,50 +33,50 @@
 #define STR(tok) STR_EXPAND(tok)
 
 /* Various lengths */
-#define HTTP_RESPONSE_TEMPLATE		"HTTP/1.1 %s\r\n" \
-									"Date: %s\r\nServer: %s\r\n" \
-									"Cache-control: no-store\r\n" \
-									"Content-Type: text/html\r\n%s\r\n" \
-									"%s"
-#define HTTP_RESPONSE_HEADER_SIZE	512
-#define HTTP_COMMAND_MAX_SIZE		1024
-#define HTTP_PROTO_MAX_SIZE			16
-#define HTTP_ARGS_MAX_SIZE			512
-#define RESPONSE_BUFFER_SIZE		4096
-#define HTTP_MAX_RESPONSE_SIZE		16386
+#define HTTP_RESPONSE_TEMPLATE      "HTTP/1.1 %s\r\n" \
+                                    "Date: %s\r\nServer: %s\r\n" \
+                                    "Cache-control: no-store\r\n" \
+                                    "Content-Type: text/html\r\n%s\r\n" \
+                                    "%s"
+#define HTTP_RESPONSE_HEADER_SIZE    512
+#define HTTP_COMMAND_MAX_SIZE        1024
+#define HTTP_PROTO_MAX_SIZE          16
+#define HTTP_ARGS_MAX_SIZE           512
+#define RESPONSE_BUFFER_SIZE         4096
+#define HTTP_MAX_RESPONSE_SIZE       16386
 
 /* HTTP codes */
-#define HTTP_CODE_OK					200
-#define HTTP_CODE_NO_CONTENT			204
-#define HTTP_CODE_NOT_FOUND				404
-#define HTTP_CODE_REQUEST_TIMEOUT 		408
-#define HTTP_CODE_REQUEST_BAD_REQUEST	400
-#define HTTP_CODE_FORBIDDEN				403
-#define HTTP_CODE_TOO_MANY_REQUESTS		429
-#define HTTP_CODE_PAYLOAD_TOO_LARGE		413
-#define HTTP_CODE_URI_TOO_LONG			414
-#define HTTP_CODE_INTERNAL_SERVER_ERROR	500
-#define HTTP_CODE_VERSION_NOT_SUPPORTED	505
-#define HTTP_CODE_INSUFFICIENT_STORAGE	507
+#define HTTP_CODE_OK                      200
+#define HTTP_CODE_NO_CONTENT              204
+#define HTTP_CODE_NOT_FOUND               404
+#define HTTP_CODE_REQUEST_TIMEOUT         408
+#define HTTP_CODE_REQUEST_BAD_REQUEST     400
+#define HTTP_CODE_FORBIDDEN               403
+#define HTTP_CODE_TOO_MANY_REQUESTS       429
+#define HTTP_CODE_PAYLOAD_TOO_LARGE       413
+#define HTTP_CODE_URI_TOO_LONG            414
+#define HTTP_CODE_INTERNAL_SERVER_ERROR   500
+#define HTTP_CODE_VERSION_NOT_SUPPORTED   505
+#define HTTP_CODE_INSUFFICIENT_STORAGE    507
 
 /* HTTP parser return codes */
-#define HTTP_PARSER_ERROR_SUCCESS				0
-#define HTTP_PARSER_ERROR_UNKNOWN_PARAM			-1
-#define HTTP_PARSER_ERROR_REQUEST_TOO_LONG 		-2
-#define HTTP_PARSER_ERROR_PARAMS_REQUIRED		-3
-#define HTTP_PARSER_ERROR_BAD_REQUEST_FORMAT 	-4
-#define HTTP_PARSER_ERROR_WRONG_PROTOCOL 		-5
-#define HTTP_PARSER_ERROR_NO_MEMORY				-6
-#define HTTP_PARSER_ERROR_DUPLICATE_PARAMS		-7
+#define HTTP_PARSER_ERROR_SUCCESS                0
+#define HTTP_PARSER_ERROR_UNKNOWN_PARAM          -1
+#define HTTP_PARSER_ERROR_REQUEST_TOO_LONG       -2
+#define HTTP_PARSER_ERROR_PARAMS_REQUIRED        -3
+#define HTTP_PARSER_ERROR_BAD_REQUEST_FORMAT     -4
+#define HTTP_PARSER_ERROR_WRONG_PROTOCOL         -5
+#define HTTP_PARSER_ERROR_NO_MEMORY              -6
+#define HTTP_PARSER_ERROR_DUPLICATE_PARAMS       -7
 
 typedef struct http_cmd_param_s {
-	char *name, *value;
+    char *name, *value;
 } http_cmd_param;
 
 typedef struct http_cmd_s {
-	char *cmdname;
-	size_t length;
-	http_cmd_param *params;
+    char *cmdname;
+    size_t length;
+    http_cmd_param *params;
 } http_cmd;
 
 /* http_cmd_param methods */
