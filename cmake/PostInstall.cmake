4e897857d65d3d5ee892c195901b5f56429beed6
Ronen Lapushner <ronenk17@gmail.com>
2017-10-22 10:29:26 +0300

    Fixed code formatting

--- cmake/PostInstall.cmake
+++ cmake/PostInstall.cmake
@@ -18,18 +18,18 @@ include(RpiwdUser.cmake)
 
 # Install init script or system unit files - depending on the init system present
 execute_process(COMMAND "python3" "${CMAKE_CURRENT_LIST_DIR}/deps/FindInitSystem.py" 
-	OUTPUT_VARIABLE INIT_SYS_NAME)
+    OUTPUT_VARIABLE INIT_SYS_NAME)
 string(REPLACE "\n" "" INIT_SYS_NAME "${INIT_SYS_NAME}")
 
 if (INIT_SYS_NAME STREQUAL "sysv" OR INIT_SYS_NAME STREQUAL "upstart")
-	file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/skel/initscripts/rpiweatherd DESTINATION /etc/init.d/rpiweatherd
-		FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
+    file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/skel/initscripts/rpiweatherd DESTINATION /etc/init.d/rpiweatherd
+        FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ GROUP_EXECUTE GROUP_READ)
 elseif (INIT_SYS_NAME STREQUAL "systemd")
-	file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/skel/initscripts/rpiweatherd.service 
-		DESTINATION /etc/systemd/system/)
+    file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/skel/initscripts/rpiweatherd.service 
+        DESTINATION /etc/systemd/system/)
 else()
-	message("Unknown/unsupported init system: ${INIT_SYS_NAME}")
-	message("No daemon automation file will be installed!")
+    message("Unknown/unsupported init system: ${INIT_SYS_NAME}")
+    message("No daemon automation file will be installed!")
 endif ()
 
 # Create the 'rpiweatherd' user
